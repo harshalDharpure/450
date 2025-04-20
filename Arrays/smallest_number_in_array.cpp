f3467b60d096630f92ff241b7119b157ffbefcba
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-04-20 18:37:13 +0530

    Create smallest_number_in_array.cpp

---
+++ Arrays/smallest_number_in_array.cpp
@@ -0,0 +1,44 @@
+
+#include<bits/stdc++.h>
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+using namespace std;
+
+int smallestarraynumber(int arr[],int n){
+int smallest=arr[0];
+for(int i=1;i<n;i++){
+  if(arr[i]<smallest){
+    smallest=arr[i];
+  }
+}
+
+return smallest;
+
+}
+
+int32_t main()
+{
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+#endif
+
+int n;
+cin>>n;
+
+int arr[n];
+
+for(int i=0;i<n;i++){
+  cin>>arr[i];
+
+}
+
+cout<<smallestarraynumber(arr,n);
+
+return 0;   
+  
+}
+
