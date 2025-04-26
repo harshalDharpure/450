8d7c5924b364e5bd069d38b44d49e63e2b23bf53
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-04-26 12:53:41 +0530

    Create access specifier.cpp

---
+++ oops /access specifier.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define int long long int
+#define F first
+#define S second
+#define pb push_back;
+
+using namespace std;
+
+class myclass{
+public:
+    int x;
+private:
+    int y;
+};
+
+int32_t main(){
+    
+ 
+#ifndef ONLINE_JUDGE
+
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#endif
+
+myclass myobj;
+
+myobj.x=25;
+myobj.y=50;
+    
+cout<<myobj.x<<"\n";
+cout<<myobj.y<<"\n";
+
+ return 0;
+ 
+}
