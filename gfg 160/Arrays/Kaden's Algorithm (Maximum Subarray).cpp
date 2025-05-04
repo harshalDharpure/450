056c126083ad76a958bafcd9298c7c937d2ec2a4
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-05-04 11:19:21 +0530

    Create Kaden's Algorithm (Maximum Subarray).cpp

---
+++ gfg 160/Arrays/Kaden's Algorithm (Maximum Subarray).cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        // Your code here
+        int max_sum = arr[0];
+        int current_sum=arr[0];
+        
+        for(int i=1;i<arr.size();i++){
+            current_sum = max(arr[i],current_sum+arr[i]);
+            max_sum= max(max_sum,current_sum);
+        }
+        return max_sum;
+        
+    }
+};
