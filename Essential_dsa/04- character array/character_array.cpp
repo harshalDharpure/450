a44c5043d71b928f4909b33590cc0d37b1900010
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-08-25 11:08:45 +0530

    Create character_array.cpp

---
+++ Essential_dsa/04- character array/character_array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char a[]="harshal";
+    cout<<a<<endl;
+
+    char b[100];
+    cin>>b;
+
+    cout<<b<<endl;
+
+    cout<<strlen(a)<<endl;
+    cout<<sizeof(a)<<endl;
+
+    return 0;
+}
