367c9dc19d86ef106da7b594069e051c77b44551
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-08-25 11:21:16 +0530

    Create cin_get.cpp

---
+++ Essential_dsa/04- character array/cin_get.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   char sentence[100];
+   char temp=cin.get();
+
+   int len=0;
+
+   while(temp!='\n'){
+    sentence[len++]=temp;
+    temp=cin.get();
+
+   }
+   sentence[len]='\0';
+   cout<<"Length "<<len<<endl;
+
+   cout<<sentence<<endl;
+   return 0;
+}
