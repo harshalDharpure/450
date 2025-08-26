2ed936797eba511d25189872d46296c0d7c75504
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-08-26 23:19:48 +0530

    Create deference_operator.cpp

---
+++ Essential_dsa/06 Pointers/deference_operator.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   //deference of the operator
+
+    int x=10;
+    int &y=x;
+    y++;
+
+    cout<<x<<endl;
+    cout<<y<<endl;
+
+    return 0;
+
+}
