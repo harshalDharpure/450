4dbd4b8c1a1438f8df94e6cdc0010e30eab8cbd8
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-08-26 13:01:20 +0530

    Create 2d_character_array.cpp

---
+++ Essential_dsa/05- 2D Array/2d_character_array.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char num[][10]={
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+    };
+
+    for(int i=0;i<5;i++){
+        cout<<num[i]<<endl;
+    }
+
+    return 0;
+}
