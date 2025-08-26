01caf6fe16674040fff35b002b32aa6659f654bd
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-08-26 23:25:05 +0530

    Create dynamic_array.cpp

---
+++ Essential_dsa/06 Pointers/dynamic_array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   
+  int n;
+  cin>>n;
+
+  int *arr= new int[n];
+  cout<<arr<<endl;
+
+  for(int i=0;i<n;i++){
+    arr[i]=i;
+    cout<<arr[i]<<" ";
+
+  }
+  cout<<endl;
+
+  delete [] arr;
+  cout<<arr<<endl;
+
+}
