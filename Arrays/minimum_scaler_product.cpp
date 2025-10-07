51f912ff692791a86b8962f4af282b968c575e7f
Harshal Dharpure <63531290+harshalDharpure@users.noreply.github.com>
2025-10-07 22:51:03 +0530

    Create minimum_scaler_product.cpp

---
+++ Arrays/minimum_scaler_product.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;
+   cin>>n;
+   vector<int> a(n),b(n);
+
+   for(int i=0;i<n;i++){cin>>a[i];}
+
+    for(int i=0;i<n;i++){cin>>b[i];}
+
+        sort(a.begin(),a.end());
+    sort(b.begin(),b.end(),greater<int>());
+
+    long long minscalerproduct=0;
+
+    for(int i=0;i<n;i++){
+        minscalerproduct+=1LL *a[i] *b[i];
+    }
+    cout<<minscalerproduct<<endl;
+
+    return 0;
+}
